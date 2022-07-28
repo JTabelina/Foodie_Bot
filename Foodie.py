648d9b9222269e0817cdab0e0de3cdb095d664ea
John Russel Tabelina <jtabelina@aceproject.space>
2022-07-27 20:05:20 -0500

    Initializing files

---
+++ Foodie.py
@@ -0,0 +1,14 @@
+food = ["Timmies", "A&W", "McDix",
+        'Subway', 'peasant cookery'
+        'dennys','smittys','coras ',
+        'stellas','ben and florentine','the original pancake house',
+        'dougieswpg','montanas', 'Saburo kitchen',
+        'sugar marmalade','crispy bunch','grand donuts',
+        'beshie takoyaki','cafe lemon tree ','Little goat food and drink /ramen  takeuma pop','Kyu snack box',
+        'St james burger and chip co (Protip, get the side poutine, not the large one)','banza noodle house','gaijin izakaya',
+        'Alfys Noodle House','Gols lanzhou noodle','Go bubble tea','Beavertails',
+        'Momo ','coffee culture','Ichiban - probably with friends ',
+        'kiwa korean cuisine restaurant','Kazoku','Banana boat',
+        'Little viet (bbt)', 'Myrnas', 'bahn mi king', 'panda tea', 'bobapaw', 
+        'little korea', 'Poké Mono', 'popeyes'
+        'wendys']
